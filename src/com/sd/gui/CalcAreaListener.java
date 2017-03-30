f3fcee393a886270b849b2fc75da0f669cd6c689
201311214 방승환 <goodjob930@naver.com>
2017-03-30 12:39:38 +0900

    Add files via upload

---
+++ src/com/sd/gui/CalcAreaListener.java
@@ -0,0 +1,21 @@
+package com.sd.gui;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+public class CalcAreaListener extends CalcAreaGUIMain implements ActionListener{
+  public void actionPerformed(ActionEvent e){
+    JButton b=(JButton)e.getSource();
+    String cmd=e.getActionCommand();
+    CalcAreaGUIMain c = new CalcAreaGUIMain();
+    if (cmd.equals("계산")) {
+            b.setText("계산했습니다");
+            Double r = Double.parseDouble(c.radiusTf1.getText());
+            c.areaTf2.setText(Double.toString(Math.PI*Math.pow(r,2)));
+        } else if (cmd.equals("취소")) {
+            b.setText("취소했습니다");
+            c.radiusTf1.setText("0");
+            c.areaTf2.setText("0");
+        }
+    }
+}
